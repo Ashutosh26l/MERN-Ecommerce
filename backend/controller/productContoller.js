d75ac9d8476b9af6c441f747b9036e6cf70a61e9
ashutosh26l <aksmba2611@gmail.com>
2025-07-24 21:16:20 +0530

    .

--- backend/controller/productContoller.js
+++ backend/controller/productContoller.js
@@ -1,3 +1,11 @@
+import Product from '../models/productModel.js';
+
+//creating products
+export const createProducts=async(req,res)=>{
+  console.log(req.body);
+  
+  //await Product.create(req,body)
+}
 export const getAllProducts=(req,res)=>{
   res.status(200).json({
     message:"All Products"
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,9 @@
 import express from'express';
-import { getAllProducts} from '../controller/productContoller.js';
+import { createProducts, getAllProducts} from '../controller/productContoller.js';
 const router = express.Router();
 
 // Routes
-
-router.route("/products").get(getAllProducts)
+router.route("/products").get(getAllProducts).post(createProducts)
 // app.post("/api/v1/products",createProduct)
 // app.get("/api/v1/product",getSingleProducts)
 // router.route("/product").get(getSingleProduct) **abhi need nahi hai**
