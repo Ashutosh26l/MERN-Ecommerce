c8b57c877f290438ab4bdc228e9bae75435aa5d6
ashutosh26l <aksmba2611@gmail.com>
2025-07-24 22:14:40 +0530

    .

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,10 @@ import express from 'express'
 import product from './routes/productRoutes.js'
 const app=express();
 
+//middle Ware
+app.use(express.json())
+
+
 // Route
 app.use("/api/v1",product)
 export default app;
--- backend/controller/productContoller.js
+++ backend/controller/productContoller.js
@@ -2,9 +2,9 @@ import Product from '../models/productModel.js';
 
 //creating products
 export const createProducts=async(req,res)=>{
-  console.log(req.body);
+  //  console.log(req.body);
   
-  //await Product.create(req,body)
+  await Product.create(req,body)
 }
 export const getAllProducts=(req,res)=>{
   res.status(200).json({
