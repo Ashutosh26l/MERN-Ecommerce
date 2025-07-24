765cb779c4ef045b45b42ed976dc46cdaf3cfeb0
ashutosh26l <aksmba2611@gmail.com>
2025-07-24 22:59:41 +0530

    .

--- backend/controller/productContoller.js
+++ backend/controller/productContoller.js
@@ -4,11 +4,17 @@ import Product from '../models/productModel.js';
 export const createProducts=async(req,res)=>{
   //  console.log(req.body);
   
-  await Product.create(req,body)
+  const product= await Product.create(req.body)
+  res.status(201).json({
+    success:true,
+    product
+  })
 }
-export const getAllProducts=(req,res)=>{
+export const getAllProducts=async(req,res)=>{
+  const products=await Product.find()
   res.status(200).json({
-    message:"All Products"
+    success:true,
+    products
   })
 }
 // export const getSingleProduct=(req, res) => {
