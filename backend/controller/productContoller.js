0dae7414ccba8d65afa51ca973a3e9dc749d5a42
ashutosh26l <aksmba2611@gmail.com>
2025-07-27 19:20:02 +0530

    continuation

--- backend/controller/productContoller.js
+++ backend/controller/productContoller.js
@@ -10,12 +10,21 @@ export const createProducts=async(req,res)=>{
     product
   })
 }
+
+//getting all products
 export const getAllProducts=async(req,res)=>{
   const products=await Product.find()
   res.status(200).json({
     success:true,
     products
   })
+}
+
+//update products
+export const updateProduct=async(req,res)=>{
+  const product =await Product.findById(req.params.id)
+  console.log(product);
+  
 }
 // export const getSingleProduct=(req, res) => {
 //   res.status(200).json({
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,10 @@
 import express from'express';
-import { createProducts, getAllProducts} from '../controller/productContoller.js';
+import { createProducts, getAllProducts, updateProduct} from '../controller/productContoller.js';
 const router = express.Router();
 
 // Routes
 router.route("/products").get(getAllProducts).post(createProducts)
+router.route("/products/:id").put(updateProduct)
 // app.post("/api/v1/products",createProduct)
 // app.get("/api/v1/product",getSingleProducts)
 // router.route("/product").get(getSingleProduct) **abhi need nahi hai**
